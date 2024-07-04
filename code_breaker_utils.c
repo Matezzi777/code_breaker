871550d2e1fe2fd3fa828766675ef679cc501fe9
Matezzi777 <matezzi777@gmail.com>
2024-07-04 18:40:58 +0200

    ft_strchr() added

--- code_breaker_utils.c
+++ code_breaker_utils.c
@@ -11,3 +11,17 @@ int ft_strlen(char *string)
         i++;
     return (i);
 }
+
+//Renvoit l'indice de la première apparition du caractère c dans la chaîne string ou -1 en cas d'absence.
+int	ft_strchr(char c, char *string)
+{
+	int	i;
+
+	while (string[i])
+	{
+		if (string[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
