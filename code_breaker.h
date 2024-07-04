6d1ec613ba95091fb1dbfbac9e0114e1fc47555a
Matezzi777 <matezzi777@gmail.com>
2024-07-04 18:40:45 +0200

    sort_list() and ft_strchr() added

--- code_breaker.h
+++ code_breaker.h
@@ -7,9 +7,11 @@
 
 //Include functions
 //code_breaker.c
-char    *code_breaker(char *string);
+char	*code_breaker(char *string);
+int		*sort_list(int	*liste);
 
 //code_breaker_utils.c
-int     ft_strlen(char *string);
+int		ft_strlen(char *string);
+int		ft_strchr(char c, char *string);
 
 #endif
